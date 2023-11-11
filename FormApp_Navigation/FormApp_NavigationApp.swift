02161963bc20d0eb2fe3574074407817ed9ff670
dzikru07 <dzikru07@gmail.com>
2023-11-11 15:46:50 +0700

    Initial Commit

---
+++ FormApp_Navigation/FormApp_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp_NavigationApp.swift
+//  FormApp_Navigation
+//
+//  Created by dzikru arya on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FormApp_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
